6607fd906bba5c230cde4c8d2caf393c62ec2476
AustrianMethod <ivanov.nv.spb@gmail.com>
2024-08-16 20:00:08 +0300

    diplom was finished

--- public/homePage.js
+++ public/homePage.js
@@ -1,14 +1,22 @@
 'use strict';
 
+
+
+/*Выход из личного кабинета*/
+
 const logoutButton = new LogoutButton();
 
 logoutButton.action = () => {
     ApiConnector.logout(data => {
         if (data.success) {   
             location.reload();
-        }      
+        }
     }); 
 };
+
+
+
+/*Получение информации о пользователе*/
 
 ApiConnector.current(response => { 
     if (response.success) { 
@@ -17,16 +25,123 @@ ApiConnector.current(response => {
 }
 );
 
+
+
+/*Получение текущих курсов валюты*/
+
 const ratesBoard = new RatesBoard();
 
-ratesBoard.getCurrencyRates = () => {
+function getCurrencyRates() {
     ApiConnector.getStocks(response => {
         if (response.success) {
-            console.log(this);
-            this.clearTable().bind(this);
-            this.fillTable(response.data).bind(this);
+            ratesBoard.clearTable();
+            ratesBoard.fillTable(response.data);
         }
     });
 }
 
-ratesBoard.getCurrencyRates();
+setTimeout(function interval() {
+        getCurrencyRates();
+        setTimeout(interval, 1000);
+    }, 1000);
+
+
+
+/*Пополнение баланса*/
+
+const moneyManager = new MoneyManager();
+
+moneyManager.addMoneyCallback = (money) => { 
+    ApiConnector.addMoney(money, (response) => {
+        const message = new FavoritesWidget();
+        if (response.success) {
+            console.log(response);
+            ProfileWidget.showProfile(response.data);
+            message.setMessage(response.success, "Баланс пополнен" );
+        } else {
+            message.setMessage(response.success, response.error);
+        }
+    });
+}
+
+
+
+/*Конвертирование валюты*/
+
+moneyManager.conversionMoneyCallback = (moneyToConvert) => {
+    ApiConnector.convertMoney(moneyToConvert, (response) => {
+        const message = new FavoritesWidget();
+        if (response.success) {
+            console.log(response);
+            ProfileWidget.showProfile(response.data);
+            message.setMessage(response.success, "Обмен выполнен" );
+        } else {
+            message.setMessage(response.success, response.error);
+        }
+    });
+}
+
+
+
+/*Перевод валюты*/
+
+moneyManager.sendMoneyCallback = (moneyToTransfer) => {
+    ApiConnector.transferMoney(moneyToTransfer, (response) => {
+        const message = new FavoritesWidget();
+        // updateUsersList();
+        if (response.success) {
+            console.log(response);
+            ProfileWidget.showProfile(response.data);
+            message.setMessage(response.success, "Перевод выполнен" );
+        } else {
+            message.setMessage(response.success, response.error);
+        }
+    });
+}
+
+
+
+/*Запрос начального списка избранного*/
+
+const favoritesWidget = new FavoritesWidget();
+
+ApiConnector.getFavorites((response) => {
+    if (response.success) { 
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data);
+    }
+});
+
+favoritesWidget.addUserCallback = (userToFavorites) => {
+    ApiConnector.addUserToFavorites(userToFavorites, (response) => {
+        const message = new FavoritesWidget();
+        if (response.success) { 
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+            message.setMessage(response.success, "Пользователь добавлен" );
+        } else {
+            message.setMessage(response.success, response.error);
+        }
+    });
+}
+
+
+
+/*Удаление пользователя из избранного*/
+
+favoritesWidget.removeUserCallback = (userToRemove) => {
+    ApiConnector.removeUserFromFavorites(userToRemove, (response) => {
+        const message = new FavoritesWidget();
+        if (response.success) { 
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+            message.setMessage(response.success, "Пользователь удален" );
+        } else {
+            message.setMessage(response.success, response.error);
+        }
+    });
+}
+
